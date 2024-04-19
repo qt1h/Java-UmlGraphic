5a96fa505ec33c036fa497164823642a0909dee2
quentinproject <girardatquentin@gmail.com>
2024-04-19 08:17:59 +0200

    deleted useless function in terminal draw

--- src/GraphicAPI/TerminalDraw.java
+++ src/GraphicAPI/TerminalDraw.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public class TerminalDraw {
 	
-	
-	
 	public class Point{
 		private int val[]=new int[2]; //allocation mémoire
 		
@@ -17,12 +15,12 @@ public class TerminalDraw {
 			this.val[1]=j;
 		}
 
-		public static void main(String[] args) {
+		/*public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			Point A = new Point(1,2);
 			Point B = new Point(3,4);
 			System.out.println(A.plus(B));
-		}
+		}*/
 		
 		public Point mult(Point b) { // x*x y*y
 			// TODO Auto-generated method stub
@@ -144,75 +142,7 @@ public class TerminalDraw {
 	            }
 	        }
 	    }
-	    
-	    public void displayEllipse() {
-	        Iterator<Integer> iter = val.iterator();
-	        while (iter.hasNext()) {
-	            int x1 = iter.next();
-	            int y1 = iter.next();
-	            int width = iter.next();
-	            int height = iter.next();
-
-	            for (int i = 0; i <= height; i++) {
-	                for (int j = 0; j <= width; j++) {
-	                    if (isInsideEllipse(j, i, width, height)) {
-	                        System.out.print("X");
-	                    } else {
-	                        System.out.print(" ");
-	                    }
-	                }
-	                System.out.println();
-	            }
-	        }
-	    }
-
-	    private boolean isInsideEllipse(int x, int y, int width, int height) {
-	        double centerX = width / 2.0;
-	        double centerY = height / 2.0;
-	        double normalizedX = (x - centerX) / centerX;
-	        double normalizedY = (y - centerY) / centerY;
-	        return (normalizedX * normalizedX + normalizedY * normalizedY) <= 1;
-	    }
-	    public void displayTriangle() {
-	        Iterator<Integer> iter = val.iterator();
-	        // Récupérer les coordonnées des sommets du triangle
-	        int x1 = iter.next();
-	        int y1 = iter.next();
-	        int x2 = iter.next();
-	        int y2 = iter.next();
-	        int x3 = iter.next();
-	        int y3 = iter.next();
-
-	        // Dessiner le triangle
-	        for (int i = 0; i <= Math.max(y1, Math.max(y2, y3)); i++) {
-	            for (int j = 0; j <= Math.max(x1, Math.max(x2, x3)); j++) {
-	                if (isInsideTriangle(j, i, x1, y1, x2, y2, x3, y3)) {
-	                    System.out.print("X");
-	                } else {
-	                    System.out.print(" ");
-	                }
-	            }
-	            System.out.println();
-	        }
-	    }
-
-	    // Vérifie si un point est à l'intérieur du triangle
-	    private boolean isInsideTriangle(int x, int y, int x1, int y1, int x2, int y2, int x3, int y3) {
-	        // Calculer les barycentres des coordonnées
-	        double denominator = ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
-	        double alpha = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / denominator;
-	        double beta = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / denominator;
-	        double gamma = 1.0 - alpha - beta;
-
-	        // Vérifier si les barycentres sont dans le triangle
-	        return alpha >= 0 && beta >= 0 && gamma >= 0;
-	    }
-
-		
 	}
-	
-	
-	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -223,10 +153,6 @@ public class TerminalDraw {
 			//Signal C = new Signal(List.of());
 			//System.out.println(A.plus(B));
 			A.displayCircle();
-			B.displayEllipse();
-			C.displayTriangle();
 			D.displayRectangle();
 		}
-	
-
 }
